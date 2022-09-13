680f0389e533b90ac98c301012042030110241d7
Eduardo Plaza <edo.plazam@gmail.com>
2022-09-12 22:48:48 -0300

    Added dynamic bg color and rotation, text and author inputs.

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
+
+  @Input() noteText: string = "";
+  @Input() noteAuthor: number = 0;    
+  get rotationStyle() { return "rotate(" + Math.floor(Math.random() * 20) + "deg)" }
+  get colorBG() { return Math.floor(Math.random() * 4) }
 
   constructor() { }
 
